d1b7b518988af4876352fa5b27eedaa55a4be110
Jason Troppens <JasonTroppens@Jasons-MacBook-Pro-4.local>
2018-01-20 00:37:06 -0500

    heres the index boys

--- Test/urls.py
+++ Test/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view.index, name ='index')
+    path('<int:char_id>/', view.char)
 ]
 
 #testing gitignore
