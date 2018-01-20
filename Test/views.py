f6d7e7b7379e86ece3d052045f12430a46db8389
Jason Troppens <JasonTroppens@Jasons-MacBook-Pro-4.local>
2018-01-20 05:10:10 -0500

    Models are all set

--- Test/views.py
+++ Test/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Character, CharInst
+
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    user = User.objects.get(pk=2)
+    return render(request, 'index.html', {'user': user})
 
 
 
 def user_info(request, user_id):
     # Test.models.objects.get(request)
-    user = User.objects.get(pk=user_id)
-    user.username
     print("Found request")
