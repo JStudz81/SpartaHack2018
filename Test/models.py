3f45ad46e5e68891eb67f4d418ad515219827a4c
Jason Troppens <JasonTroppens@Jasons-MacBook-Pro-4.local>
2018-01-20 03:19:38 -0500

    Fixing model stuff

--- Test/models.py
+++ Test/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.username
-
 
 class Character(models.Model):
 
