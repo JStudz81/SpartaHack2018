bb4c1887f9a37cb9af998a3debd8351ff569e5f9
Jason Troppens <JasonTroppens@Jasons-MacBook-Pro-4.local>
2018-01-20 02:09:16 -0500

    trying to fix the path import

--- Test/views.py
+++ Test/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+
 def user_info(request):
-    StatSmash.models.objects.get(request)
+    # Test.models.objects.get(request)
     print("Found request")
+    return
