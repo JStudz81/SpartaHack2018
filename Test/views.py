a4d990db7b101cdd6e11426fd2f354fdfaecfd25
jslivka66 <slivkajo@msu.edu>
2018-01-20 03:17:53 -0500

    Update models with Jason

--- Test/views.py
+++ Test/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import User
 
 def index(request):
     return render(request, 'index.html', {})
 
 def user_info(request, user_id):
+    user = User.objects.get(pk=1)
+    user.username
+    print("request received")
+    pass
